e383cff32934f0947a7498ca6832c9004beb123c
Bruno Martins Correa <brunomsft@gmail.com>
2023-08-05 10:16:04 -0300

    Adds openapi endpoint

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConsoleLogger } from '@nestjs/common';
 import { ErrorFilter } from './common/filter/error.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-const APP_NAME = 'App';
+const APP_NAME = 'API';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
@@ -12,6 +13,16 @@ async function bootstrap() {
       logLevels: ['verbose', 'debug', 'log', 'warn', 'error'],
     }),
   });
+
+  const documentBuilder = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.setGlobalPrefix('api').useGlobalFilters(new ErrorFilter()).listen(8080);
 }
