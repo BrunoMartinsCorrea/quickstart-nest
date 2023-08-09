8bf2669056ed0fa35a7b2d15f064ae33102b7f17
Gustavo Oquendo Nogales <gustavo.nogales@hotmail.com>
2023-08-09 18:11:20 -0300

    change vite default port to 8081

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -6,6 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   server: {
+    port: 8081,
     proxy: {
       '/api': {
         target: 'http://localhost:8080',
