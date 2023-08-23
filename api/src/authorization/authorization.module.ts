ce1053d9d8868dd106fccb886330fed0a61b0032
Bruno Martins Correa <brunomsft@gmail.com>
2023-08-23 20:26:17 -0300

    Removes unused imports

--- api/src/authorization/authorization.module.ts
+++ api/src/authorization/authorization.module.ts
@@ -3,13 +3,12 @@ import { RoleController } from './http-server/controller/role.controller';
 import { RoleService } from '@/authorization/domain/service/role.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RoleEntity } from '@/authorization/persistence/entity/role.entity';
-import { RoleGroupEntity } from '@/authorization/persistence/entity/role-group.entity';
 import { RoleRepository } from '@/authorization/persistence/repository/role-repository';
 
 @Module({
   controllers: [RoleController],
   providers: [RoleService, RoleRepository],
-  imports: [TypeOrmModule.forFeature([RoleEntity, RoleGroupEntity])],
+  imports: [TypeOrmModule.forFeature([RoleEntity])],
   exports: [RoleService],
 })
 export class AuthorizationModule {}
